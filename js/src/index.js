90dcd80186ff8b3529648a11ad3194dc8a54ba4d
Luca Bacchi <bacchilu@gmail.com>
2022-02-24 15:18:35 +0100

    Refactoring

--- js/src/index.js
+++ js/src/index.js
@@ -3,23 +3,44 @@ import ReactDOM from 'react-dom';
 
 import {Modal} from './modal';
 
-const App = function (props) {
-    const onClick = function () {
-        Modal.render(
-            'Hello Dante',
-            <figure className="text-end">
-                <blockquote className="blockquote">
-                    <p>
-                        Nel mezzo del cammin di nostra vita
-                        <br />
-                        mi ritrovai per una selva oscura,
-                        <br />
-                        ché la diritta via era smarrita.
-                    </p>
-                </blockquote>
-                <figcaption className="blockquote-footer">anonymous Florentine</figcaption>
-            </figure>
+const terzine = [
+    ['Nel mezzo del cammin di nostra vita', 'mi ritrovai per una selva oscura,', 'ché la diritta via era smarrita.'],
+    [
+        'Ahi quanto a dir qual era è cosa dura',
+        'esta selva selvaggia e aspra e forte',
+        'che nel pensier rinova la paura!',
+    ],
+    [
+        'Tant’è amara che poco è più morte;',
+        'ma per trattar del ben ch’i’ vi trovai,',
+        'dirò de l’altre cose ch’i’ v’ ho scorte.',
+    ],
+];
+
+const ModalBody = function () {
+    const terzinaRandom = terzine[Math.floor(Math.random() * terzine.length)];
+    const content = terzinaRandom.map(function (t, index) {
+        return (
+            <span key={index}>
+                {t}
+                <br />
+            </span>
         );
+    });
+
+    return (
+        <figure className="text-end">
+            <blockquote className="blockquote">
+                <p>{content}</p>
+            </blockquote>
+            <figcaption className="blockquote-footer">anonymous Florentine</figcaption>
+        </figure>
+    );
+};
+
+const App = function () {
+    const onClick = function () {
+        Modal.render('Hello Dante', <ModalBody />);
     };
 
     return (
